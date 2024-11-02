95d93d9378015b2018a1009546034cd25fada921
Gregory Noel Marcello <gregorynoelmarcellosoetanto_2327240045@mhs.mdp.ac.id>
2024-11-02 08:51:39 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,8 @@ class MyHomePage extends State<MyHomePage> {
       color: Colors. blue,
       height: 200, padding: EdgeInsets.all(16),
       margin: EdgeInsets.all(16),
-      child : Center
-    )
-    ],
+      child : Center('Explore the charm of our cits hidden gems and embark on a journey to dicover the rich cultural heritage, breathtaking landscapes, and unique experiences that await you From historic landmarks to vibrant markets and serene natural wonders, our city offers a tapestry of experiences that will captivate your senses. Join us in exploring the beauty of our city and create memories that will last a lifetime. Come and visit our citys treasures today.'
+
     ),
     )
     )
